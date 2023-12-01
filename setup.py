506a765890f0c9d5575d21331d70dcfbb06fdfad
Alex Shafer <ashafer@pm.me>
2023-11-30 20:26:27 -0800

    packaging

--- setup.py
+++ setup.py
@@ -3,9 +3,15 @@ from setuptools import setup
 setup(
     name='python-datapath',
     version='0.1.0',
+
+    author='Alex Shafer',
+    author_email='ashafer@pm.me',
     description='Functions for working with dotted and square-bracketed paths against a recursive dict/list structure',
-    packages=['datapath'],
+    url='https://github.com/ashafer01/python-datapath',
+    license='MIT',
+
     python_requires='>=3.10.0',
+    packages=['datapath'],
     install_requires=[
         'regex',
     ],
